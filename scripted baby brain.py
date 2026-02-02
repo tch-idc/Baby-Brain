ef5b42197c763283c8fdeb31bfa8ce1238ba3f91
Jarif <tchidc.ig@gmail.com>
2026-02-02 16:29:25 +0600

    scripted brain v1
    0 parameters

---
+++ scripted baby brain.py
@@ -0,0 +1,23 @@
+class BabyAI:
+    def __init__(self):
+        self.memory = []
+        self.mood = "neutral"
+
+    def learn(self, text):
+        self.memory.append(text)
+
+    def think(self):
+        if self.mood == "happy":
+            return "I feel good today 😊"
+        elif self.mood == "angry":
+            return "I don't like that 😡"
+        else:
+            return "I am learning..."
+
+baby = BabyAI()
+
+baby.learn("Fire is hot")
+baby.learn("Sky is blue")
+
+print(baby.think())
+print("Memory:", baby.memory)
